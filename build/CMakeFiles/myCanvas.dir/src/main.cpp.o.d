05a539d532515bcd156b097dff3782cbbebe8af5
drjam2005 <jamesallanmdr2005@gmail.com>
2026-01-10 18:41:09 +0800

    removed builds and such

--- build/CMakeFiles/myCanvas.dir/src/main.cpp.o.d
+++
@@ -1,4 +0,0 @@
-CMakeFiles/myCanvas.dir/src/main.cpp.o: \
- /home/james/Projects/myCanvas/src/main.cpp /usr/include/stdc-predef.h \
- /home/james/Projects/myCanvas/include/raylib.h \
- /usr/lib/gcc/x86_64-pc-linux-gnu/15.2.1/include/stdarg.h
