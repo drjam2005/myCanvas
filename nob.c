180f16621483e72b746087f1babdda7ba010813a
drjam2005 <jamesallanmdr2005@gmail.com>
2026-07-16 17:49:15 +0800

    chore: fixed linking error for linux (through WSL Ubuntu)

--- nob.c
+++ nob.c
@@ -237,7 +237,7 @@ int main(int argc, char** argv){
 #elif defined(__linux__)
 		"-Lvendor/raylib/unix/lib/", "-l:libraylib.a",
 		"-Lvendor/SDL/unix/lib/", "-lSDL3",
-		"-lX11",
+		"-lX11", "-lXi"
 #endif
 	};
 
